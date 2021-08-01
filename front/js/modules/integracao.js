e8245ee59077ef371fbe2afc5bdbdaf72d247b05
Rick <rodriperep@alu.ufc.br>
2021-07-31 22:58:37 -0300

    :construction: Implementação de Local Storage
    Precisa ser refatorado para melhor compreensão

--- front/js/modules/integracao.js
+++ front/js/modules/integracao.js
@@ -1,8 +1,16 @@
 let token = '';
 let formsView = document.querySelector(".forms");
+if(document.querySelector(".forms")){
+     
+}
+let logoutButton = document.getElementById('logout');
+
+if (localStorage.getItem('TOKEN-SESSION')){
+   logIn();
+}
 
 const logout = () => {
-    token = ''
+    token = '';
 }
 
 function registerPost(url, body){
@@ -11,11 +19,9 @@ function registerPost(url, body){
     request.setRequestHeader("Content-type", "application/json");
     request.send(JSON.stringify(body));
     request.onload = function(){
-        console.log(this.responseText);
-        token = this.responseText
-        console.log(token)
-    }
-    
+    token = this.responseText
+    localStorage.setItem("TOKEN-SESSION", token);
+    }   
     return request.responseText;
 }
 function registerUser(){
@@ -26,8 +32,6 @@ function registerUser(){
     let email = document.getElementById("email-register").value ;
     let senha = document.getElementById("password-register").value;
     let endereco = document.getElementById("adress-register").value;
-    console.log(nome, login, email, senha, endereco);
-
     let body = {"endereco": endereco,
         "usuario": {
         "adm": "false",
@@ -42,22 +46,46 @@ function registerUser(){
 function loginUser(){
     event.preventDefault();
     let url = "http://localhost:8181/login";
-    let login =  document.getElementById("login-user").value;
-    let senha = document.getElementById("password-login").value;
-    
-
-    let body = {"login" : login,
-        "senha" : senha,
+    let login =  document.getElementById('login-user').value;
+    let senha = document.getElementById('password-login').value;
+    localStorage.setItem('LOGIN-SESSION', login);
+    localStorage.setItem('SENHA-SESSION', senha);  
+    let body = {'login' : login,
+        'senha' : senha,
     }
     registerPost(url, body);
 
     if(token !== ''){
-        formsView.classList.add("form-disabled");
+        if(document.querySelector(".forms")){
+            formsView.classList.add("form-disabled");
+        }
     }
+    
 }
 
 if (document.querySelector("#nomeMyInfo")){
     let nomeUser = document.querySelector("#nomeMyInfo");
     console.log(nomeUser.innerHTML);
     nomeUser.innerHTML = "meu nome";
+}
+
+function logIn(){
+    if(document.querySelector(".forms")){
+        formsView.classList.add('form-disabled');
+    }
+    logoutButton.classList.add("is-active");
+    let userLoging = localStorage.getItem('LOGIN-SESSION');
+    let user = document.getElementById('user');
+    user.innerHTML = userLoging;      
+}
+
+function logOut(){
+    localStorage.clear();
+    logoutButton.classList.remove("is-active");
+    let user = document.getElementById('user');
+    user.innerHTML = "";
+    if (document.querySelector(".forms")){
+        formsView.classList.add('form-enable');
+    }
+    
 }
