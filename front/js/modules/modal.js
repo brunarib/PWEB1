d943abdea9eb3c3ca730c83fc2a91a4dd3f7dab7
nicholastre <66094502+nicholastre@users.noreply.github.com>
2021-08-18 11:16:03 -0300

    Ajustar modal

--- front/js/modules/modal.js
+++ front/js/modules/modal.js
@@ -1,19 +1,19 @@
 if(document.querySelector('.button-modal')){
     let btnModal = document.querySelector('.button-modal');
+    
+    let containerModal = document.querySelector('[data-modal="container"]');
+    let closeModal = document.querySelector('[data-modal="close"]');
 
     btnModal.addEventListener('click', openModal);
     closeModal.addEventListener('click', modalClose);
+    function openModal(){
+        event.preventDefault();
+        containerModal.classList.add('is-active');
+    }
+    
+    function modalClose(){
+        event.preventDefault();
+        containerModal.classList.remove('is-active');
+    }
 }
 
-const containerModal = document.querySelector('[data-modal="container"]');
-const closeModal = document.querySelector('[data-modal="close"]');
-
-function openModal(){
-    event.preventDefault();
-    containerModal.classList.add('is-active');
-}
-
-function modalClose(){
-    event.preventDefault();
-    containerModal.classList.remove('is-active');
-}
