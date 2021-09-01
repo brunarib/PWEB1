f26a0e500aaa3e3548fa6527fb3b607f628baed2
Rick <rodriperep@alu.ufc.br>
2021-08-31 23:50:37 -0300

    Problemas
    Mudado para adicionar e diminuir, mas os botões não estão funcionais

--- front/js/modules/crud-carrinho.js
+++ front/js/modules/crud-carrinho.js
@@ -1,6 +1,37 @@
-var guardaItens = [];
-localStorage.setItem("CARRINHO", guardaItens);
-
+if (localStorage.getItem("CARRINHO") === null){
+    var guardaItens = [];
+    localStorage.setItem("CARRINHO", JSON.stringify(guardaItens));
+}
 function addItemCarrinho(){
-    console.log("Add");
+    let productName = document.querySelector('.title__item').innerHTML;
+    let productPrice = document.querySelector('.prices__total').innerHTML;    
+    let dados = JSON.stringify({
+        nome : productName,
+        preco : productPrice
+    });
+    console.log(dados);
+    let guarda = JSON.parse(localStorage.getItem('CARRINHO'));
+    guarda.push(dados);
+    localStorage.setItem("CARRINHO", JSON.stringify(guarda));
+}
+function mostraIteCarrinho(){
+    console.log("Mostra");
+}
+function aumenta(){
+    let quantidade = document.querySelector(".quantidade-carrinho").textContent;
+    let valor = document.querySelector(".preco-carrinho").textContent;
+    quantidade = Number(quantidade) + 1; 
+    valor = Number(quantidade) * Number(valor);
+}
+function diminui(){
+    let quantidade = document.querySelector(".quantidade-carrinho").textContent;
+    let valor = document.querySelector(".preco-carrinho").textContent;
+    if (Number(quantidade) >= 1){
+        quantidade = Number(quantidade) - 1; 
+        String(valor).innerHTML = Number(quantidade) * Number(valor);
+        quantidade.innerHTML = String(quantidade);
+    }
+    else{
+        alert("Operação Invalida");
+    }  
 }
