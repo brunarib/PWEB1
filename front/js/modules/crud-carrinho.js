8e92abb1b58f123ed8f911ebc49e6027b684816a
Rick <rodriperep@alu.ufc.br>
2021-09-01 14:27:45 -0300

    Ajustado o add cart

--- front/js/modules/crud-carrinho.js
+++ front/js/modules/crud-carrinho.js
@@ -2,21 +2,41 @@ if (localStorage.getItem("CARRINHO") === null){
     var guardaItens = [];
     localStorage.setItem("CARRINHO", JSON.stringify(guardaItens));
 }
-function addItemCarrinho(){
-    let productName = document.querySelector('.title__item').innerHTML;
-    let productPrice = document.querySelector('.prices__total').innerHTML;    
+const addButtonCarrinho = () =>{
+    const add = document.createElement('button');
+    add.innerText = "Comprar";
+    add.addEventListener('click', addProduto);
+    return add;
+}
+const addProduto = (event) =>{
+    const add = event.target;
+    let currentName = add.parentElement.querySelector('.title__item').innerHTML;
+    let currentPreco =add.parentElement.querySelector('.prices__total').innerHTML;
+    console.log(currentName);
+    console.log(currentPreco);
+    addCart(currentName, currentPreco);
+    return add;
+}
+function addCart(nome, preco){
+    let productName = nome;
+    let productPrice = preco;
     let dados = JSON.stringify({
         nome : productName,
-        preco : productPrice
-    });
-    console.log(dados);
-    let guarda = JSON.parse(localStorage.getItem('CARRINHO'));
-    guarda.push(dados);
-    localStorage.setItem("CARRINHO", JSON.stringify(guarda));
+        preco :  productPrice
+        });
+        console.log(dados);
+        let guarda = JSON.parse(localStorage.getItem('CARRINHO'));
+        guarda.push(dados);
+        localStorage.setItem("CARRINHO", JSON.stringify(guarda));
+    alert("Produto adicionado ao Carrainho");         
 }
+/*
 function mostraIteCarrinho(){
     console.log("Mostra");
 }
+
+
+
 function aumenta(){
     let quantidade = document.querySelector(".quantidade-carrinho").textContent;
     let valor = document.querySelector(".preco-carrinho").textContent;
@@ -35,3 +55,12 @@ function diminui(){
         alert("Operação Invalida");
     }  
 }
+
+function adicionaCarinho(nome, preco){
+    
+    
+}
+
+
+
+*/
